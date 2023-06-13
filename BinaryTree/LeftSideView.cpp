36ba2cf9a359018916dd7ee96b2b26cc9af1d17e
Shubham Kumar <shubhamedx26@gmail.com>
2023-06-13 16:34:37 +0530

    Create LeftSideView.cpp

---
+++ BinaryTree/LeftSideView.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void leftView(TreeNode* root, vector<int> &ans, int level){
+        if(root == NULL) return;
+        if(ans.size() == level) ans.push_back(root->val);
+
+        leftView(root->left, ans, level+1);
+        leftView(root->right, ans, level+1);
+    }
+    vector<int> leftSideView(TreeNode* root) {
+        vector<int> ans;
+        leftView(root, ans, 0);
+        return ans;
+    }
+};
