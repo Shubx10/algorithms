0b3976229bfbde9bb1c3d0d7a4cab92f0cee47c8
Shubham Kumar <shubhamedx26@gmail.com>
2023-07-03 00:24:30 +0530

    Create DFS.cpp

---
+++ Graph/DFS.cpp
@@ -0,0 +1,34 @@
+void preparedAdjList(unordered_map<int, vector<int>> &adjList, vector<pair<int, int>> &edges){
+    for(int i = 0; i < edges.size(); ++i){
+        int u = edges[i].first;
+        int v = edges[i].second;
+      
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+}
+
+void dfs(unordered_map<int, vector<int>> &adjList, unordered_map<int, bool> &visited, vector<int> &ans, int node){
+    visited[node] = 1;
+    ans.push_back(node);
+
+    for(auto i: adjList[node]){
+        if(!visited[i]){
+            dfs(adjList, visited, ans, i);
+        }
+    }
+}
+
+vector<int> DFS(int vertex, vector<pair<int, int>> edges){
+    unordered_map<int, vector<int>> adjList;
+    vector<int> ans;
+    unordered_map<int, bool> visited;
+    
+    preparedAdjList(adjList, edges);
+    for(int i = 0; i < vertex; ++i){
+        if(!visited[i]){
+            dfs(adjList, visited, ans, i); 
+        }
+    }
+   return ans; 
+}
