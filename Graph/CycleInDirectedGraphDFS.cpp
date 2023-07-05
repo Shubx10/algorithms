a3f434d150437717830072dedcc3a0be8b2b34ee
Shubham Kumar <shubhamedx26@gmail.com>
2023-07-05 13:46:18 +0530

    Create CycleInDirectedGraphDFS.cpp

---
+++ Graph/CycleInDirectedGraphDFS.cpp
@@ -0,0 +1,40 @@
+bool checkCycleDFS(int node, vector<bool> &visited, vector<bool> &dfsvisited, vector<vector<int>> &adj){
+    visited[node] = true;
+    dfsvisited[node] = true;
+    
+    for(auto neighbour: adj[node]){
+        if(!visited[neighbour]){
+            bool cycledetected = checkCycleDFS(neighbour, visited, dfsvisited, adj);
+            if(cycledetected){
+                return true;
+            }
+        }
+        else if(dfsvisited[neighbour]){
+            return true;
+        }
+    }
+    dfsvisited[node] = false;
+    return false;
+}
+
+int detectCycleInDirectedGraph(int v, vector<pair<int, int>> &edges){
+    vector<vector<int>> adj(v);
+    vector<bool> visited(v);
+    vector<bool> dfsvisited(v);
+
+    for(int i = 0; i < edges.size(); ++i){
+        int u = edges[i].first;
+        int v = edges[i].second;
+        adj[u].push_back(v);
+    }
+    
+    for(int i = 0; i < v; ++i){
+        if(!visited[i]){
+            bool cyclefound = checkCycleDFS(i, visited, dfsvisited, adj);
+            if(cyclefound){
+                return true;
+            }
+        }
+    }
+    return false;
+}
