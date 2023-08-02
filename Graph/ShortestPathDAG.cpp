c3274fc6bd3b6adbe754270760d8fe6d42a769d9
Shubham Kumar <shubhamedx26@gmail.com>
2023-08-02 21:37:54 +0530

    Update ShortestPathDAG.cpp

--- Graph/ShortestPathDAG.cpp
+++ Graph/ShortestPathDAG.cpp
@@ -1,11 +1,11 @@
 // DAG - Directed Acyclic Graph (weighted)
-class Solution {
+class Solution{
 public:
     void topoSort(int node, vector<vector<pair<int, int>>> adj, vector<bool> &vis, stack<int> &st){ 
         vis[node] = 1;
-        for (auto it: adj[node]) {
+        for(auto it: adj[node]){
             int v = it.first;
-            if (!vis[v]) {
+            if(!vis[v]){
                 topoSort(v, adj, vis, st);
             }
         }
@@ -32,22 +32,22 @@ public:
         }
 
         dist[0] = 0;
-        while (!st.empty()) {
+        while(!st.empty()){
             int node = st.top();
             st.pop();
         
-            for (auto it: adj[node]) {
+            for(auto it: adj[node]){
                 int v = it.first;
                 int wt = it.second;
             
-                if (dist[v] > dist[node] + wt){
+                if(dist[v] > dist[node] + wt){
                     dist[v] = wt + dist[node];
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            if (dist[i] == 1e9) dist[i] = -1;
+        for(int i = 0; i < N; i++){
+            if(dist[i] == 1e9) dist[i] = -1;
         }
         return dist;
     }
