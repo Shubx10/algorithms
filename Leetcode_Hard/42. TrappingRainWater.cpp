b5a1b2e8d7342c2e3978ac7e0d81f914c1ad825a
Shubham Kumar <shubhamedx26@gmail.com>
2024-04-12 13:04:59 +0530

    Update 42. TrappingRainWater.cpp

--- Leetcode_Hard/42. TrappingRainWater.cpp
+++ Leetcode_Hard/42. TrappingRainWater.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int> maxR(height.size()), maxL(height.size());
-        maxR[0] = height[0];
-        maxL[maxL.size()-1] = height[height.size()-1];
-        for(int i=1; i<height.size(); ++i){
-            maxR[i] = max(maxR[i-1], height[i]);
-        }
-        for(int i=height.size()-2; i>=0; --i){
-            maxL[i] = max(maxL[i+1], height[i]);
-        }
+        int n = height.size();
         int area = 0;
-        for(int i=0; i<height.size(); ++i){
+        vector<int> maxR(n), maxL(n);
+        maxR[0] = height[0];
+        maxL[n - 1] = height[n - 1];
+        for(int i = 1; i < n; ++i){
+            maxR[i] = max(maxR[i - 1], height[i]);
+        }
+        for(int i = n - 2; i >= 0; --i){
+            maxL[i] = max(maxL[i + 1], height[i]);
+        }
+        for(int i = 0; i < n; ++i){
             area += min(maxR[i], maxL[i]) - height[i];
         }
         return area;
