2e948a644a650e016c0763106b25799fac1e2446
Shubham Kumar <shubhamedx26@gmail.com>
2024-06-05 10:33:33 +0530

    Update LeftSideView.cpp

--- BinaryTree/LeftSideView.cpp
+++ BinaryTree/LeftSideView.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     void leftView(TreeNode* root, vector<int> &ans, int level){
         if(root == NULL) return;
-        if(ans.size() == level) ans.push_back(root->val);
-
-        leftView(root->left, ans, level+1);
-        leftView(root->right, ans, level+1);
+        if(ans.size() == level){
+            ans.push_back(root->val);
+        }
+        leftView(root->left, ans, level + 1);
+        leftView(root->right, ans, level + 1);
     }
     vector<int> leftSideView(TreeNode* root) {
         vector<int> ans;
