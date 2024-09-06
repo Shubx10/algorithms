4c032224d86242b355ddf6ec2cf8b946128c0809
Shubham Kumar <shubhamedx26@gmail.com>
2024-09-06 18:34:21 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,37 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void traverse(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void traverse(int arr[], int n){
+    for(int i = 0; i < n; ++i){
         cout << arr[i] << " ";
     }
 }
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n){
     bool swapped;
-    for (int i = 0; i < n - 1; i++) {
+    for(int i = n - 1; i > 0; --i){
         swapped = false;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]){
+        for(int j = 0; j < i; ++j){
+            if(arr[j] > arr[j + 1]){
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        if (swapped == false)
-            break;
+        if(swapped == false) break;
     }
 }
 
-int main()
-{
+int main(){
     int arr[5] = {36, 2, 45, 25, 18};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Unsorted Array - \n";
     traverse(arr, n);
     bubbleSort(arr, n);
-
     cout << "\nSorted Array - \n";
     traverse(arr, n);
-
     return 0;
 }
