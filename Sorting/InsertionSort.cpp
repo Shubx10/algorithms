2f459ad28dc63dbce2c6a5985b7117658291d2f1
Shubham Kumar <shubhamedx26@gmail.com>
2024-09-06 18:48:23 +0530

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -2,26 +2,20 @@
 using namespace std;
 
 void insertionSort(int arr[], int n){
-    int i, j;
-    for(i = 1; i < n; i++){
-        int var = arr[i];
-        for(j = i - 1; j >= 0; j--){
-            if(arr[j] > var){
-                arr[j + 1] = arr[j];
-            }
-            else{
-                break;
-            }
+    for(int i = 0; i < n; ++i){
+        int j = i;
+        while(j > 0 && arr[j - 1] > arr[j]){
+            swap(arr[j - 1], arr[j]);
+            j--;
         }
-        arr[j + 1] = var;
     }
 }
 
 int main(){
-    int arr[] = { 9, 6, 7, 2, 5, 8};
+    int arr[] = {9, 6, 7, 2, 5, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
     insertionSort(arr, n);
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; ++i){
         cout << arr[i] << " ";
     }
     return 0;
